0e8e5dd967ffddaafcfe42d815be753070a8fbc8
Jitendra Shukla <jitushukla@ymail.com>
2024-11-22 21:48:31 +0530

    Added Recipe page, form for user input

--- Recipe/urls.py
+++ Recipe/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app1.views import *
 
 urlpatterns = [
+    # path('',home, name ="home"),
+    path('', recipe, name="recipe"),
     path('admin/', admin.site.urls),
 ]
--- app1/views.py
+++ app1/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import *
+
 
 # Create your views here.
+def recipe(request):
+    if request.method == "POST":
+        data = request.POST
+        recipe_name = data.get("recipe_name")
+        recipe_description = data.get("recipe_description")
+        recipe_image = request.FILES.get('recipe_image')
+        Recipe.objects.create(
+            recipe_name = recipe_name,
+            recipe_description =  recipe_description,
+            recipe_image= recipe_image)
+
+        return redirect("/")
+        # return redirect("/recipe/")
+    return render(request, "recipe.html")
