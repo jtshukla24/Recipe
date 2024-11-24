92fab7c605ffd5d6428e5d35d77f99b429a66e42
Jitendra Shukla <jitushukla@ymail.com>
2024-11-24 23:34:28 +0530

    Added Register and login page, and User model

--- Recipe/urls.py
+++ Recipe/urls.py
@@ -17,9 +17,22 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from app1.views import *
+from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     # path('',home, name ="home"),
     path('', recipe, name="recipe"),
     path('admin/', admin.site.urls),
+    path('delete_recipe/<id>/', delete_recipe, name="delete_recipe"),
+    path('update_recipe/<id>/', update_recipe, name="update_recipe"),
+    path('login/',login_page, name = "login_page"),
+    path('register/', register, name="register")
+
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += staticfiles_urlpatterns()
--- app1/views.py
+++ app1/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponse
 from .models import *
-
+from django.contrib.auth.models import User
 
 # Create your views here.
 def recipe(request):
@@ -10,10 +11,65 @@ def recipe(request):
         recipe_description = data.get("recipe_description")
         recipe_image = request.FILES.get('recipe_image')
         Recipe.objects.create(
-            recipe_name = recipe_name,
-            recipe_description =  recipe_description,
-            recipe_image= recipe_image)
+            recipe_name=recipe_name,
+            recipe_description=recipe_description,
+            recipe_image=recipe_image)
 
         return redirect("/")
         # return redirect("/recipe/")
-    return render(request, "recipe.html")
+    queryset = Recipe.objects.all()
+
+    if request.GET.get('search'):
+        queryset = queryset.filter(recipe_name__icontains=request.GET.get('search'))
+
+    context = {'recipes': queryset}
+    return render(request, "recipe.html", context)
+
+
+def delete_recipe(request, id):
+    queryset = Recipe.objects.get(id=id)
+    queryset.delete()
+    return redirect('/')
+
+
+def update_recipe(request, id):
+    queryset = Recipe.objects.get(id=id)
+    if request.method == "POST":
+        data = request.POST
+        recipe_name = data.get("recipe_name")
+        recipe_description = data.get("recipe_description")
+        recipe_image = request.FILES.get('recipe_image')
+
+        queryset.recipe_name = recipe_name
+        queryset.recipe_description = recipe_description
+
+        if queryset.recipe_image:
+            queryset.recipe_image = recipe_image
+        queryset.save()
+        return redirect('/')
+
+    context = {'recipes': queryset}
+    return render(request, "update_recipe.html", context)
+
+
+def login_page(request):
+    if request.method == "POST":
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = User.objects.create(
+            first_name = first_name,
+            last_name = last_name,
+            username = username,
+        )
+        user.set_password(password)
+        user.save()
+
+        return redirect('/register/')
+    return render(request, "login.html")
+
+
+def register(request):
+    return render(request, "register.html")
