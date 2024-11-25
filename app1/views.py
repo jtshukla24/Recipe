7bcc64ffad0a6410258fe69bbc705b236dde50ff
Jitendra Shukla <jitushukla@ymail.com>
2024-11-25 22:49:37 +0530

    Added Advanced authentication in User Model

--- app1/views.py
+++ app1/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import *
 from django.contrib.auth.models import User
+from django.contrib import messages
+
 
 # Create your views here.
 def recipe(request):
@@ -60,9 +62,9 @@ def login_page(request):
         password = request.POST.get('password')
 
         user = User.objects.create(
-            first_name = first_name,
-            last_name = last_name,
-            username = username,
+            first_name=first_name,
+            last_name=last_name,
+            username=username,
         )
         user.set_password(password)
         user.save()
@@ -72,4 +74,25 @@ def login_page(request):
 
 
 def register(request):
+    if request.method == "POST":
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = User.objects.filter(username=username)
+        if user.exists():
+            messages.info(request,'username already exists')
+            return redirect('/register/')
+
+        user = User.objects.create(
+            first_name=first_name,
+            last_name=last_name,
+            username=username,
+        )
+        user.set_password(password)
+        user.save()
+        messages.info(request,'account created successfully')
+
+        return redirect('/register/')
     return render(request, "register.html")
