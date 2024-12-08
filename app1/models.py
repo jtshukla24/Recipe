4e49d52705e5e90514052e0e27df1ce6bdf9c0c1
Jitendra Shukla <jitushukla@ymail.com>
2024-12-08 21:54:28 +0530

    Changed Models, admin files

--- app1/models.py
+++ app1/models.py
@@ -11,35 +11,3 @@ class Recipe(models.Model):
     recipe_image = models.ImageField(upload_to="recipe_images")
     recipe_view_count = models.IntegerField(default=1)
 
-
-class Department(models.Model):
-    department = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.department
-
-    class Meta:
-        ordering = ['department']
-
-
-class StudentID(models.Model):
-    student_id = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.student_id
-
-
-class Student(models.Model):
-    department = models.ForeignKey(Department, related_name="depart", on_delete=models.CASCADE)
-    student_id = models.OneToOneField(StudentID, related_name="studentid", on_delete=models.SET_NULL, null=True)
-    student_name = models.CharField(max_length=100)
-    student_email = models.EmailField(unique=True)
-    student_age = models.IntegerField(default=10)
-    student_address = models.TextField()
-
-    def __str__(self):
-        return self.student_name
-
-    class Meta:
-        ordering = ['student_name']
-        verbose_name = "student"
